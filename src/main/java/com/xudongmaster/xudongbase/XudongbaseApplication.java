69d9a50100fe08ee25102ff86654847d2c4629c1
Eden <2218555734@qq.com>
2024-04-09 16:46:50 +0800

    项目初始化

---
+++ src/main/java/com/xudongmaster/xudongbase/XudongbaseApplication.java
@@ -0,0 +1,13 @@
+package com.xudongmaster.xudongbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XudongbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XudongbaseApplication.class, args);
+    }
+
+}
